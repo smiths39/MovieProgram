ba1c174bc5faac0eaa838f642305ba62323d340a
Sean Smith <seansmith@Seans-MacBook-Pro.local>
2018-03-18 19:35:27 +0000

    added files back

---
+++ Movie.java
@@ -0,0 +1,34 @@
+public class Movie {
+    public static void main(String [] args) {
+        String [] movieArray = {
+            "Who's That Knocking At My Door",
+            "Boxcar Bertha",
+            "Mean Streets",
+            "Alice Doesn't Live Here Anymore",
+            "Taxi Driver", 
+            "New York, New York",
+            "Raging Bull", 
+            "The King Of Comedy",
+            "After Hours",
+            "The Color Of Money",
+            "The Last Temptation Of Christ",
+            "Goodfellas", 
+            "Cape Fear",
+            "The Age Of Innocence",
+            "Casino", 
+            "Kundun",
+            "Bringing Out The Dead",
+            "Gangs Of New York",
+            "The Aviator",
+            "The Departed",
+            "Shutter Island",
+            "Hugo",
+            "The Wolf Of Wall Street",
+            "Silence",
+            "The Irishman"};
+
+        for (String title : movieArray) {
+            System.out.println(title);
+        }
+    }
+}
